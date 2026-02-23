91c1012eb789f7f9655bd43663f6011f5ec985f3
Xantibody <zeku.bushinryu38@gmail.com>
2026-02-23 15:34:27 +0900

    feat: automate Firefox addon and flake.lock updates
    Use mozilla-addons-to-nix for generating addon definitions from
    addons.json. Add GitHub Actions workflows for daily Firefox addon
    updates and weekly flake.lock updates via pull requests authenticated
    with GitHub App tokens.

--- default.nix
+++ default.nix
@@ -15,7 +15,25 @@
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
-  firefox-addons = pkgs.callPackage ./pkgs/firefox-addons { };
+  firefox-addons = pkgs.lib.recurseIntoAttrs (
+    pkgs.callPackage ./pkgs/firefox-addons {
+      buildFirefoxXpiAddon = pkgs.lib.makeOverridable ({ pname, version, addonId, url, sha256, meta, ... }:
+        pkgs.stdenv.mkDerivation {
+          name = "${pname}-${version}";
+          inherit meta;
+          src = pkgs.fetchurl { inherit url sha256; };
+          preferLocalBuild = true;
+          allowSubstitutes = true;
+          passthru = { inherit addonId; };
+          buildCommand = ''
+            dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+            mkdir -p "$dst"
+            install -v -m644 "$src" "$dst/${addonId}.xpi"
+          '';
+        }
+      );
+    }
+  );
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
--- pkgs/firefox-addons/default.nix
+++ pkgs/firefox-addons/default.nix
@@ -1,39 +1,26 @@
-{
-  fetchurl,
-  lib,
-  stdenv,
-}:
-
-let
-  buildFirefoxXpiAddon =
-    {
-      pname,
-      version,
-      addonId,
-      url,
-      sha256,
-    }:
-    stdenv.mkDerivation {
-      inherit pname version;
-
-      src = fetchurl { inherit url sha256; };
-
-      preferLocalBuild = true;
-      allowSubstitutes = true;
-
-      buildCommand = ''
-        dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-        mkdir -p "$dst"
-        install -v -m644 "$src" "$dst/${addonId}.xpi"
-      '';
+{ buildFirefoxXpiAddon, fetchurl, lib, stdenv }:
+  {
+    "plamo-translate" = buildFirefoxXpiAddon {
+      pname = "plamo-translate";
+      version = "0.3.11";
+      addonId = "{da1a7868-5062-4eb9-a787-6d9210703929}";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4662921/plamo_translate-0.3.11.xpi";
+      sha256 = "a4884d5d7f53a03e2b3b7fd583ba90fdfb0dd426abb6a97e057dc7870ebae97d";
+      meta = with lib;
+      {
+        homepage = "https://translate.preferredai.jp";
+        description = "A Firefox extension for translating web pages by PLaMo Translate Model.";
+        mozPermissions = [
+          "storage"
+          "activeTab"
+          "tabs"
+          "identity"
+          "contextMenus"
+          "webNavigation"
+          "<all_urls>"
+          "https://app.translate.preferredai.jp/browser/callback*"
+        ];
+        platforms = platforms.all;
+      };
     };
-in
-{
-  "plamo-translate" = buildFirefoxXpiAddon {
-    pname = "plamo-translate";
-    version = "0.3.2";
-    addonId = "{da1a7868-5062-4eb9-a787-6d9210703929}";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4631720/plamo_translate-0.3.2.xpi";
-    sha256 = "sha256-QPxmsLVZc4USvDPEdAl2vgVXZzHQwTsaNA1ZzgwdK8c=";
-  };
-}
+  }
